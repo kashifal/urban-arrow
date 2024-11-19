9c6a6c2118c85dfd5706c07fb1f4dadf478055f2
kashifal <captainkashi4@gmail.com>
2024-11-20 03:30:43 +0500

    fuck

---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+// src/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+ 
+// Retrieve the language from localStorage or use default 'en'
+const storedLang = localStorage.getItem('language') || 'en';
+
+i18n
+   
+  .use(initReactI18next) // Initializes i18next for React
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "welcome": "Welcome",
+          "hello": "Hello",
+        },
+      },
+      es: {
+        translation: {
+          "welcome": "Bienvenido",
+          "hello": "Hola",
+        },
+      },
+    },
+    lng: storedLang, // Set the initial language from localStorage
+    fallbackLng: 'en', // Fallback language
+    interpolation: {
+      escapeValue: false, // React already escapes values
+    },
+  });
+
+export default i18n;
