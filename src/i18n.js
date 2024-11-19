c695d315e03674853215befa5ade7832fa02057e
kashifal <captainkashi4@gmail.com>
2024-11-20 03:40:36 +0500

    language

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,11 @@
 // src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
- 
+
 // Retrieve the language from localStorage or use default 'en'
 const storedLang = localStorage.getItem('language') || 'en';
 
 i18n
-   
   .use(initReactI18next) // Initializes i18next for React
   .init({
     resources: {
@@ -20,6 +19,12 @@ i18n
         translation: {
           "welcome": "Bienvenido",
           "hello": "Hola",
+        },
+      },
+      fr: {
+        translation: {
+          "welcome": "Bienvenue",
+          "hello": "Bonjour",
         },
       },
     },
